6cdef19c00fa8123fa4e0bd6dc374b6d38b1f9af
valdoyk <osvaldovillalba-02@hotmail.com>
2025-04-13 01:08:18 -0600

    Fix astro config 🫠

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: 'https://valdoyking.is-a.dev',
-    base: '/portafolio',
+    site: 'https://valdoyking.github.io',
+    /* base: '/portafolio', */
 });
