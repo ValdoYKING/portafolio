532bbe452b342b696c9df4a49e40b0d56ba590fa
valdoyk <osvaldovillalba-02@hotmail.com>
2025-04-13 01:01:11 -0600

    Add domain is-a.dev :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: 'https://valdoyking.github.io',
+    site: 'https://valdoyking.is-a.dev',
     base: '/portafolio',
 });
